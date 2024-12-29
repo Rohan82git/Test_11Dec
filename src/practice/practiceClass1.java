210678d408cb95f78705c8ead4c43a5ddd77590a
RohanKhochare <RDKhochare@outlook.com>
2024-12-29 18:56:40 +0530

    This is final

--- src/practice/practiceClass1.java
+++ src/practice/practiceClass1.java
@@ -4,8 +4,8 @@ public class practiceClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello_class1_branchA changes,Second time Branch A changes done my Rohan D Khochare");
 		System.out.println("Branch B changes,Second time branch B changes"+"This time this changes done in Branch A");
+		System.out.println("Hello_class1_branchA changes"+"This is done in Branch B");		
 	}
 
 }
