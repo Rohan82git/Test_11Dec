5040b1f10ab3c711e23e8964d06f2dc2640f1fb6
RohanKhochare <RDKhochare@outlook.com>
2024-12-29 23:33:06 +0530

    Second

--- src/practice/practiceClass1.java
+++ src/practice/practiceClass1.java
@@ -4,7 +4,7 @@ public class practiceClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Rohan D Khochare cretaed it in local"+"Hello java");
+		System.out.println("Rohan D Khochare cretaed it in local"+"Hello java"+"Hii world");
 		System.out.println("Hello_class1_branchA changes"+"This is done in Branch B");		
 	}
 
