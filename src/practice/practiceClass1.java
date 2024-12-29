4181292fbce16910f2cbae4318ad8e7c69b58fbf
RohanKhochare <RDKhochare@outlook.com>
2024-12-29 17:48:23 +0530

    Rohan_changes

--- src/practice/practiceClass1.java
+++ src/practice/practiceClass1.java
@@ -4,7 +4,7 @@ public class practiceClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello_class1_branchA changes,second time Branch A changes");
+		System.out.println("Hello_class1_branchA changes,Second time Branch A changes done my Rohan");
 		System.out.println("Branch B changes");
 	}
 
